2b331ba07e5aa5f8f15b1345bf2bee30f08010ba
rick_barkley <rick_barkley@nuvola-networks.com>
2013-12-19 17:26:46 -0600

    migrated to bootstrap3 and major style overhaul

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ EDesign::Application.routes.draw do
   root :to => 'pages#home'
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
+  match '/home', :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
