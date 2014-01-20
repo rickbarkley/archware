d8a461cdf02112e3b6c2d984db06edb33964093f
rick_barkley <rick_barkley@nuvola-networks.com>
2014-01-20 12:07:46 -0600

    added sortable functionality

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -5,7 +5,7 @@ class PlansController < ApplicationController
   def index
 
     @q = Plan.search(params[:q])
-    @plans = @q.result(distinct: true).paginate(:page => params[:page], :per_page => 10)
+    @plans = @q.result(distinct: true).order(params[:sort]).paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erbs
