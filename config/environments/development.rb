cf8f93d6a9dd98cce054d33988960a6fb148deeb
rick_barkley <rick_barkley@nuvola-networks.com>
2014-03-01 06:55:53 -0600

    updated mailer and about page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,11 +53,12 @@ EDesign::Application.configure do
 
 
   config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.mailer_sender = "info@nuvola-networks.com"
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
 end
